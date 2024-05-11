6ddba65a44a8cd0a7d70809df0abfc9debc53c60
LaBoulange <la.boulange.tezos@gmail.com>
2024-05-11 07:07:03 +0200

    Bumped expeced protocol name and Serokell's expected version number

--- usr/local/bin/tezos-constants.sh
+++ usr/local/bin/tezos-constants.sh
@@ -4,9 +4,9 @@
 # Version variables for octez
 ##############################
 
-export PROTOCOL="Proxford"
-export PROTOCOL_VERSION="19.2-1"
-export PROTOCOL_FORMER="PtNairob"
+export PROTOCOL="PtParisB"
+export PROTOCOL_VERSION="20.0-1"
+export PROTOCOL_FORMER="Proxford"
 
 export OCTEZ_DOWNLOAD_URL="https://github.com/serokell/tezos-packaging/releases/download/v${PROTOCOL_VERSION}/binaries-${PROTOCOL_VERSION}.tar.gz"
 export ZCASH_DOWNLOAD_URL="https://download.z.cash/downloads"
