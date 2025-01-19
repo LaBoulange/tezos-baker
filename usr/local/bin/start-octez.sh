d8b8692874347ee6fae41dbc3e580cc1bbf0f1b9
LaBoulange <la.boulange.tezos@gmail.com>
2025-01-19 23:12:13 +0100

    minor refactoring

--- usr/local/bin/start-octez.sh
+++ usr/local/bin/start-octez.sh
@@ -8,7 +8,7 @@ echo "Starting RPC node"
 nohup octez-node run --config-file=$NODE_CONFIG_FILE --rpc-addr $NODE_RPC_ADDR --log-output=$NODE_LOG_FILE &>/dev/null &
 
 echo "Checking RPC node bootstrap"
-sleep 20
+sleep 30
 octez-client --base-dir $CLIENT_BASE_DIR --endpoint http://${NODE_RPC_ADDR} bootstrapped
 
 echo "Starting DAL node"
@@ -16,7 +16,7 @@ nohup octez-dal-node run &>$DAL_LOG_FILE &
 
 echo "Checking DAL node bootstrap"
 sleep 5
-curl http://localhost:10732/p2p/points/info?connected
+curl http://${DAL_ENDPOINT_ADDR}/p2p/points/info?connected
 
 echo "Starting baker"
 nohup octez-baker-${PROTOCOL} --base-dir $CLIENT_BASE_DIR --endpoint http://${NODE_RPC_ADDR} run with local node $NODE_RUN_DIR $KEY_BAKER --liquidity-baking-toggle-vote $BAKER_LIQUIDITY_BAKING_SWITCH --dal-node http://${DAL_ENDPOINT_ADDR} &>$BAKER_LOG_FILE &
