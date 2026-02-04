ccc82fe15edd34f5def54b734458d16c0776a2b6
LaBoulange <la.boulange.tezos@gmail.com>
2026-02-04 07:46:32 +0100

    Install from branch

--- usr/local/bin/install-tezos-baker.sh
+++ usr/local/bin/install-tezos-baker.sh
@@ -7,7 +7,38 @@ ARCHIVE='tezos.baker.tar.gz'
 INSTALL_USER=`whoami`
 INSTALL_GROUP=`groups | awk '{print $1}'`
 
-TEZOS_BAKER_DOWNLOAD_URL=`wget https://api.github.com/repos/LaBoulange/tezos-baker/releases/latest -O - | sed -nr 's/\s*"tarball_url":\s*"(.*)",/\1/p'`
+# Function to display usage message
+show_usage() {
+    echo "Usage: $0 [--branch BRANCH_NAME]"
+    echo "  --branch: Specify a branch to install from (default: latest release)"
+    exit 1
+}
+
+# Parse command line arguments
+BRANCH=""
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --branch)
+            if [ -z "$2" ] || [[ "$2" == --* ]]; then
+                show_usage
+            fi
+            BRANCH="$2"
+            shift 2
+            ;;
+        *)
+            show_usage
+            ;;
+    esac
+done
+
+# Determine download URL based on whether --branch was specified
+if [ -n "$BRANCH" ]; then
+    echo "Installing from branch: $BRANCH"
+    TEZOS_BAKER_DOWNLOAD_URL="https://github.com/LaBoulange/tezos-baker/archive/refs/heads/${BRANCH}.tar.gz"
+else
+    echo "Installing from latest release"
+    TEZOS_BAKER_DOWNLOAD_URL=`wget https://api.github.com/repos/LaBoulange/tezos-baker/releases/latest -O - | sed -nr 's/\s*"tarball_url":\s*"(.*)",/\1/p'`
+fi
 
 THIS_FILE_NAME='install-tezos-baker.sh'
 
